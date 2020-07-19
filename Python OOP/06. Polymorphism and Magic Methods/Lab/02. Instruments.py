944ebff7d439df6dfa465b14caa608f49030c95f
peterkaragyozov <pepitoitalianeca@yahoo.com>
2020-07-20 01:27:37 +0300

    Added Polymorphism and Magic Methods Lab

---
+++ Python OOP/06. Polymorphism and Magic Methods/Lab/02. Instruments.py
@@ -0,0 +1,20 @@
+def play_instrument(instrument):
+    return instrument.play()
+
+
+class Guitar:
+    def play(self):
+        print("playing the guitar")
+
+
+class Piano:
+    def play(self):
+        print("playing the piano")
+
+
+guitar = Guitar()
+play_instrument(guitar)
+
+piano = Piano()
+play_instrument(piano)
+
