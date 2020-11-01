9d93fc58c5910b988f9929c5bbfd103c580a2b75
peterkaragyozov <pepitoitalianeca@yahoo.com>
2020-11-01 16:25:50 +0200

    Petstagram Project

---
+++ Python Web Basics/02. Petstagram Project/pets/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from pets.models import Pet, Like, Comment
+
+
+class LikeInLine(admin.TabularInline):
+    model = Like
+
+
+class PetAdmin(admin.ModelAdmin):
+    # fields = ('type', 'name')
+    list_display = ('id', 'type', 'name', 'age')
+    list_filter = ('type', 'age',)
+    inlines = (
+        LikeInLine,
+    )
+
+
+admin.site.register(Pet, PetAdmin)
+admin.site.register(Like)
+admin.site.register(Comment)
