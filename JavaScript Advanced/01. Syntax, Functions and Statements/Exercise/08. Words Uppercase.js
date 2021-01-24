af5704491affa331acb6ce8b6851d29c62071554
Peter Karagyozov <pepitoitalianeca@yahoo.com>
2021-01-25 01:06:59 +0200

    Added missing solution

---
+++ JavaScript Advanced/01. Syntax, Functions and Statements/Exercise/08. Words Uppercase.js
@@ -0,0 +1,9 @@
+function input(string) {
+    let result = string
+                 .toUpperCase()
+                 .match(/\w+/g);
+ 
+    console.log(result.join(', '));
+}
+
+input('Hey, how are you?')
