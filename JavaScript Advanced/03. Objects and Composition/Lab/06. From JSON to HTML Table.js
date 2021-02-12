3fbbfb4c41c840f6209b982f7bd079d41d1c0c03
Peter Karagyozov <pepitoitalianeca@yahoo.com>
2021-02-12 16:27:31 +0200

    Updated Solution

--- JavaScript Advanced/03. Objects and Composition/Lab/06. From JSON to HTML Table.js
+++ JavaScript Advanced/03. Objects and Composition/Lab/06. From JSON to HTML Table.js
@@ -1,20 +1,42 @@
-function fromJSONToHTMLTable(input) {
-    let output = '<table>\n   <tr>'
-    let jsonAsObj = JSON.parse(input)
+function solve(input) {
+    let parse = JSON.parse(input)
+    let str = '<table>\n'
+    str += `   <tr>${Object.keys(parse[0]).map(x => `<th>${replace(x)}</th>`).join('')}</tr>\n` 
+    parse.forEach( x => { str += `   <tr>${Object.values(x).map( x => `<td>${replace(x)}</td>`).join('')}</tr>\n` })
+    str += '</table>'
 
-    for (let row of Object.keys(jsonAsObj[0])) {
-        output += '<th>' + row + '</th>'
+    
+    function replace(str) {
+        return String(str)
+            .replace(/&/g , '&amp;')
+            .replace(/</g , "&lt;")
+            .replace(/>/g , "&gt;")
+            .replace(/"/g , "&quot;")
+            .replace(/'/g , "&#39;")
     }
-
-    output += '</tr>\n'
-
-    jsonAsObj.forEach((obj) => {
-        let values = Object.values(obj);
-        output += '   <tr>';
-        values.forEach((val) => output += `<td>${val}</td>`)
-        output += '</tr>\n';
-    })
-
-    output += '</table>'
-    console.log(output)
+    return str
 }
+
+
+// Second way
+
+// function fromJSONToHTMLTable(input) {
+//     let output = '<table>\n   <tr>'
+//     let jsonAsObj = JSON.parse(input)
+
+//     for (let row of Object.keys(jsonAsObj[0])) {
+//         output += '<th>' + row + '</th>'
+//     }
+
+//     output += '</tr>\n'
+
+//     jsonAsObj.forEach((obj) => {
+//         let values = Object.values(obj);
+//         output += '   <tr>';
+//         values.forEach((val) => output += `<td>${val}</td>`)
+//         output += '</tr>\n';
+//     })
+
+//     output += '</table>'
+//     console.log(output)
+// }
